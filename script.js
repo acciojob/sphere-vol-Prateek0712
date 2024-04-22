0fa55d4ea53e5060ba2a82c87e00e6176d7a4255
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-22 13:34:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+  let r=document.getElementById('radius').value;
+	let result=((4/3)*(Math.PI)*(r*r*r));
+	let vol=document.getElementById('volume');
+	vol.value=result;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
